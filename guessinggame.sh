c1d63e813361ed9b5c0b018c63d61c5a9d289c2b
1234-ritu <67013490+1234-ritu@users.noreply.github.com>
2020-10-26 17:58:26 +0530

    Create guessinggame.sh

---
+++ guessinggame.sh
@@ -0,0 +1,18 @@
+#File: guessinggame.sh
+
+function game{
+     while[[$guess_number -ne $total_files]]
+     do
+         if[[$guess_number -lt $total_files]]
+         then
+             echo"Your Guess is too low."
+         else
+             echo"Your Guess is too high."
+         fi
+         done
+         echo"Congratulations, You have guessed right."
+}
+echo "Guess how many files are there in the current directory."
+read guess_number
+total_files=$(ls | wc -l)
+game
